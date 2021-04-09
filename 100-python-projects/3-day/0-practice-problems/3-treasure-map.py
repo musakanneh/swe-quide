048533251ea7046ecacbf8749ce50d6e419f8e5a
musakanneh <mkanneh17@alustudent.com>
2021-04-09 03:32:06 +0200

    Day four completed

--- 100-python-projects/3-day/0-practice-problems/3-treasure-map.py
+++ 100-python-projects/3-day/0-practice-problems/3-treasure-map.py
@@ -7,16 +7,18 @@ def treasure_map():
     """
     row_1 = ['⬜️', '⬜️', '⬜️']
     row_2 = ['⬜️', '⬜️', '⬜️']
-    row_3 = ['⬜️', '⬜️', 'x']
+    row_3 = ['⬜️', '⬜️', '⬜️']
 
     row_map = [row_1, row_2, row_3]
     print("{}\n{}\n{}".format(row_1, row_2, row_3))
     position = input("Where do you want to put the treasure?: ")
 
-    vertical = int(position[0])
-    horizontal = int(position[1])
+    horizontal = int(position[0])
+    vertical = int(position[1])
 
-    print(row_map[vertical - 1])
-    
+    selected_row = row_map[vertical - 1][horizontal - 1] = " X"
+
     print("{}\n{}\n{}".format(row_1, row_2, row_3))
+
+
 print(treasure_map())
---
+++ 100-python-projects/3-day/1-project/README.md
@@ -0,0 +1,2 @@
+# ROCK PAPER SCISSORS PROFESSIONALLY
+Everyone wants to be a professional athlete, but who wants to wake up early and go to the gym. Not to mention all that time you have to spend worrying about perfecting your diet to compete at the highest level. Rock Paper Scissors avoids all this. There are benefits to studying strategy and practicing but when it comes down to it: on any given day anyone can emerge Victorious in Rock Paper Scissors.
