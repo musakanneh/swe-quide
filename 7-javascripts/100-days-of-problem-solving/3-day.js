897d328381e0f6eec34d9352759aefe69d226ea4
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2022-03-27 09:19:25 +0100

    Hot fix

--- 7-javascripts/100-days-of-problem-solving/3-day.js
+++ 7-javascripts/100-days-of-problem-solving/3-day.js
@@ -3,16 +3,16 @@
  * @param {number[]} arr
  * @return {number}
  */
- var sumOddLengthSubarrays = function(arr) {
+var sumOddLengthSubarrays = function (arr) {
     let sum = 0;
-    
+
     for (let i = 0; i < arr.length; i++) {
         for (let j = i; j < arr.length; j++) {
             let sub_arr_len = j - i + 1;
             let is_sub_arr_odd = sub_arr_len % 2 !== 0;
-            if (!is_sub_arr_odd) {
-                continue;
-            }
+
+            if (!is_sub_arr_odd) { continue }
+            
             for (let k = i; k <= j; k++) {
                 sum += arr[k];
             }
---
+++ 7-javascripts/100-days-of-problem-solving/6-day.js
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/rank-transform-of-an-array/
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function (arr) {
+
+};
